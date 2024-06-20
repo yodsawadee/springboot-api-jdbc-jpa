6e8df113478d4b4be4d9f72df0126beb1b1562df
yodsawadee <jan.yodsawadee@outlook.com>
2024-06-20 18:45:39 +0700

    call external api to get data and insert

---
+++ src/main/java/org/example/ApiDataModel.java
@@ -0,0 +1,54 @@
+package org.example;
+
+
+public class ApiDataModel {
+    private int userId;
+    private int id;
+    private String title;
+    private String body;
+
+    public ApiDataModel() {
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    @Override
+    public String toString() {
+        return "ApiDataModel{" +
+                "userId=" + userId +
+                ", id=" + id +
+                ", title='" + title + '\'' +
+                ", body='" + body + '\'' +
+                '}';
+    }
+}
